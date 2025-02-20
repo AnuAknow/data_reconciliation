1658626ebc012828ee15c0d5581e5cc32cde0c6d
AnuAknow <everettaknowledge50@gmail.com>
2025-02-19 18:46:41 -0800

    compare_csv.py refactor

--- compare_csv.py
+++ compare_csv.py
@@ -1,37 +1,11 @@
-import csv
-from openpyxl import Workbook
+import pandas as pd
+data_path = 'data\\'
+csv_file1 = data_path + 'Plexxis_Tax Details_Ck_Date_121523.csv'
+csv_file2 = data_path + 'Zenefits_payroll_detail_Ck_Date_121523.csv'
 
-def compare_csv(file1_path, file2_path, output_file_path="differences.xlsx"):
-    """
-    Compares two CSV files and writes the differences to an Excel file.
+df1 = pd.read_csv(csv_file1)
+df2 = pd.read_csv(csv_file2)
 
-    Args:
-        file1_path (str): Path to the first CSV file.
-        file2_path (str): Path to the second CSV file.
-        output_file_path (str, optional): Path to save the Excel output. Defaults to "differences.xlsx".
-    """
-    with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
-        reader1 = csv.reader(file1)
-        reader2 = csv.reader(file2)
+common = df1.merge(df2,on=['Last Name','First Name'])
+print(common)
 
-        # Store differences as tuples (row_index, row_from_file1, row_from_file2)
-        differences = []
-        row_index = 0
-        for row1, row2 in zip(reader1, reader2):
-            if row1 != row2:
-                differences.append((row_index, row1, row2))
-            row_index += 1
-
-    # Create a new workbook and select the active sheet
-    workbook = Workbook()
-    sheet = workbook.active
-    sheet.title = "Differences"
-
-    # Write headers
-    sheet.append(["Row Index", "File 1 Row", "File 2 Row"])
-
-    # Write differences to the sheet
-    for diff in differences:
-        # Convert lists to strings before appending
-        diff = (diff[0], str(diff[1]), str(diff[2]))
-        sheet.append(diff)
